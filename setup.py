242fb07102cb3abc00e87c8611fe187ff044f4d9
Sean Harkins <sharkinsgis@gmail.com>
2020-08-13 14:12:51 -0500

    Use 1.4 schema.

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name="hls_manifest",
     version="0.1",
     packages=find_packages(),
-    install_requires=["click", ],
+    install_requires=["click", "jsonschema"],
+    include_package_data=True,
     extras_require={"dev": ["flake8", "black"], "test": ["flake8", "pytest"]},
     entry_points={"console_scripts": ["create_manifest=hls_manifest.hls_manifest:main", ]},
 )
